2950a88d1d230f96cc0602e3e7dde2c0377e206b
Howar-Yick <hao.yick@gmail.com>
2025-09-26 16:40:07 +0800

    Fix Kotlin plugin version conflict

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,8 @@
 // yicksync/android/build.gradle.kts (项目级)
 
 plugins {
-    id("com.android.application") version "8.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.android.application") apply false
+    id("org.jetbrains.kotlin.android") apply false
     id("dev.flutter.flutter-gradle-plugin") apply false
 }
 
